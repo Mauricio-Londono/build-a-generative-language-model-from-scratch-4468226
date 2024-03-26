aa2cdb7d158742c4b286b418a19a285c359e1c42
Mauricio-Londono <131040921+Mauricio-Londono@users.noreply.github.com>
2024-03-26 21:15:34 +0000

    Learning completed

--- 02/02_02_begin.py
+++ 02/02_02_begin.py
@@ -42,6 +42,16 @@ class NaiveBayesClassifier:
         pos = []
         neg = []
 
+        for token in tokens:
+            pos.append(self.pos_counter[token]/self.sample_count)
+            neg.append(self.neg_counter[token]/self.sample_count)
+
+        if sum(pos) > sum(neg):
+            return "Positive."
+        elif sum(neg) > sum(pos):
+            return "Negative"
+        else:
+            return "Neutral"           
 
 
 
--- 03/03_02_begin.py
+++ 03/03_02_begin.py
@@ -9,3 +9,18 @@ CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
 
 with open(CURRENT_DIR + "/word_to_vector_trsf.pkl", "rb") as pk:
     word_to_vector = pickle.load(pk)
+
+def cosine_similarity(vec_a, vec_b):
+    numerator = [vec_a[i] * vec_b[i] for i in range(len(vec_a))]
+    denominator = (norm(vec_a) * norm(vec_b))
+    return numerator/denominator
+
+def similar_words(word="tree", top_k=10):
+    return sorted(
+        word_to_vector.keys(), 
+        key=lambda x: -cosine_similarity(word_to_vector[x], word_to_vector[word])
+    )[:top_k]
+
+print(cosine_similarity(word_to_vector["plant"], word_to_vector["grow"]))
+print(cosine_similarity(word_to_vector["minute"], word_to_vector["plant"]))
+print(cosine_similarity(word_to_vector["plant"], word_to_vector["tree"]))
